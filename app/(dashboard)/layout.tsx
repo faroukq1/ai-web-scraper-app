ad06ba2a4f9f75375d2a59e313a348b01c6a0c6a
faroukq1 <ouledmeriemfarouk1@gmail.com>
2025-08-03 16:02:21 +0100

    feat: implement all clerk functionality

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import BreadcrumbHeader from '@/components/BreadcrumbHeader';
 import DesktopSidebar from '@/components/Sidebar';
 import { ModeToggle } from '@/components/ThemeModeToggle';
 import { Separator } from '@/components/ui/separator';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -10,8 +11,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           <BreadcrumbHeader />
-          <div className="gap-1 flex items-center">
+          <div className="gap-4 flex items-center">
             <ModeToggle />
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
         </header>
         <Separator />
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/sign-in">
+    <ClerkProvider
+      afterSignOutUrl="/sign-in"
+      appearance={{
+        elements: {
+          formButtonPrimary:
+            'bg-primary hover:bg-primary/90 text-sm !shadow-none',
+        },
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
